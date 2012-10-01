0660972fb410187491cf905b9c0ff611df6fc09d
Victor Varvaryuk <v.varvariuc@corp.mail.ru>
2012-10-01 10:46:06 +0400

    .

--- packages/vic/fonts/__init__.py
+++ packages/vic/fonts/__init__.py
@@ -2,9 +2,11 @@
 install_package('fonts-droid')
 update_config('./kdeglobals', '~/.kde/share/config/kdeglobals')
 request_config_reload()
+del('~/.fonts.conf')  # or patch and then move it to:
 cp('./fonts.conf', '~/.config/fontconfig/')
+cp('./fonts.conf', '~/.fonts.conf')  # in 12.04 only this works
 """
-If a font in the browser in not Dorid, in Google Chrome right click on the text with the worng font,
+If a font in the browser in not Droid, in Google Chrome right click on the text with the worng font,
 select 'Inspect element', find 'Computed style' and 'font-family' in it:
 
 font-family: 'lucida grande', tahoma, verdana, arial, sans-serif;
@@ -16,5 +18,5 @@ LiberationSans-Regular.ttf: "Liberation Sans" "Regular"
 
 Ok, you found the offending font. Add it to 'fonts.conf' file.
 """
-def override_font(font, override):
+def override_font(font, override, strong_binding=True):
     """Add necessary nodes to fonts.conf """
