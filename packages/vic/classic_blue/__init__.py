efa0467042eb71a21ed9f1cca46a87d25db679a7
Victor <victor.varvariuc@gmail.com>
2013-01-08 19:16:41 +0400

    In classic_blue action icons were not copied.

--- packages/vic/classic_blue/__init__.py
+++ packages/vic/classic_blue/__init__.py
@@ -14,6 +14,7 @@ lines as possible - to make it visually light.
     def proceed(self):
         self.copy_file('./classic_blue.qtcurve', '~/.kde/share/apps/QtCurve/')
         self.copy_file('./classic_blue.colors', '~/.kde/share/apps/color-schemes/')
+        self.copy_file('./Hycons', '~/.kde/share/icons/')
         self.update_kconfig('./stylerc', '~/.config/qtcurve/stylerc')
         self.update_kconfig('./kwinrc', '~/.kde/share/config/kwinrc')
         self.update_kconfig('./kwinqtcurverc', '~/.kde/share/config/kwinqtcurverc')
