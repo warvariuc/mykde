f52cd28d372698653a295172313887486e071793
Victor <victor.varvariuc@gmail.com>
2013-09-08 20:54:02 +0400

    Fixed Session Locking global keyboard shortcut

--- packages/vic/global_shortcuts/__init__.py
+++ packages/vic/global_shortcuts/__init__.py
@@ -4,7 +4,7 @@ from mykde import BaseAction
 class Action(BaseAction):
 
     name = "Global keyboard shortcuts"
-    description = """\
+    description = """
 <ul>
     <li><kbd>Meta+L</kbd> - Lock screen</li>
     <li><kbd>Meta+D</kbd> - Show desktop</li>
--- packages/vic/wallpaper/__init__.py
+++ packages/vic/wallpaper/__init__.py
@@ -32,11 +32,7 @@ This work by <a href="http://si.smugmug.com">Simon Tong</a> is licensed under a
             'org.kde.ActivityManager', '/ActivityManager/Activities')
         current_activity_id = dbus.Interface(
             activity_manager, 'org.kde.ActivityManager.Activities').CurrentActivity()
-#        print('Current activity ID:', current_activity_id)
-        
-#        kwin = dbus.SessionBus().get_object('org.kde.kwin', '/KWin')
-#        print('Current desktop:', dbus.Interface(kwin, 'org.kde.KWin').currentDesktop())
-        
+
         konf_path = self.make_abs_path('~/.kde/share/config/plasma-desktop-appletsrc')
         # http://api.kde.org/pykde-4.7-api/kdecore/KConfig.html
         konf = KConfig(konf_path, KConfig.SimpleConfig)
@@ -45,49 +41,6 @@ This work by <a href="http://si.smugmug.com">Simon Tong</a> is licensed under a
             group = containments.group(group_name)
             # http://api.kde.org/pykde-4.7-api/kdecore/KConfigGroup.html
             if group.readEntry('activityId') == current_activity_id:
-#                print('Containment ID of the current activity:', group_name)
                 wallpaper_image_group = group.group('Wallpaper').group('image')
                 wallpaper_image_group.writeEntry('wallpaper', wallpaper_path)
 
-
-
-#import sys
-#import os
-#import subprocess
-#
-#import dbus
-#from PyQt4 import QtGui
-#from PyKDE4.kdecore import KConfig
-#
-#
-#activity_manager = dbus.SessionBus().get_object('org.kde.kactivitymanagerd', '/ActivityManager')
-#current_activity_id = dbus.Interface(activity_manager, 'org.kde.ActivityManager').CurrentActivity()
-#print('Current activity ID:', current_activity_id)
-#
-#kwin = dbus.SessionBus().get_object('org.kde.kwin', '/KWin')
-#print('Current desktop:', dbus.Interface(kwin, 'org.kde.KWin').currentDesktop())
-#
-#print('Primary screen:', QtGui.QApplication(sys.argv).desktop().primaryScreen())
-#
-#konf_path = os.path.expanduser('~/.kde/share/config/plasma-desktop-appletsrc')
-## http://api.kde.org/pykde-4.7-api/kdecore/KConfig.html
-#konf = KConfig(konf_path, KConfig.SimpleConfig)
-#containments = konf.group('Containments')
-#for group_name in containments.groupList():
-#    group = containments.group(group_name)
-#    # http://api.kde.org/pykde-4.7-api/kdecore/KConfigGroup.html
-#    if group.readEntry('activityId') == current_activity_id:
-#        print('Containment ID of the current activity:', group_name)
-#        wallpaper_image_group = group.group('Wallpaper').group('image')
-#        wallpaper_image_group.writeEntry('wallpaper', '/home/vic/.kde/share/wallpapers/morning 3 machu picchu peru-X3.jpg')
-#
-#
-## dbus call does not cause wallpaper change, i guess applet config is not re-read
-## plasma = dbus.SessionBus().get_object('org.kde.plasma-desktop', '/MainApplication')
-## dbus.Interface(plasma, 'org.kde.KApplication').reparseConfiguration()
-#
-#print('Restarting plasma...')
-#with open(os.devnull, "w") as fnull:
-#    subprocess.call('kquitapp plasma-desktop && sleep 2s', shell=True, stdout=fnull, stderr=fnull)
-#    konf.sync()  # save the configuration
-#    subprocess.call('plasma-desktop &', shell=True, stdout=fnull, stderr=fnull)
