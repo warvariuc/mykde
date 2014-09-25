91ed53313b11e403b8db3ca38140e51468b5765e
Victor Varvaryuk <victor.varvariuc@gmail.com>
2014-09-25 17:45:16 +0400

    Updated QtCurve theme

--- packages/vic/qtcurve_vic/__init__.py
+++ packages/vic/qtcurve_vic/__init__.py
@@ -21,8 +21,8 @@ lines as possible - to make it visually light.<br>
 
     def _proceed(self, **kwargs):
         self.print_text('Installing %r' % self.name)
-        self.copy_file('./classic_blue.qtcurve', '~/.kde/share/apps/QtCurve/')
-        self.copy_file('./classic_blue.colors', '~/.kde/share/apps/color-schemes/')
+        self.copy_file('./vic.qtcurve', '~/.kde/share/apps/QtCurve/')
+        self.copy_file('./vic_dark.colors', '~/.kde/share/apps/color-schemes/')
         self.update_kconfig('./stylerc', '~/.config/qtcurve/stylerc')
         self.update_kconfig('./kwinrc', '~/.kde/share/config/kwinrc')
         self.update_kconfig('./kwinqtcurverc', '~/.kde/share/config/kwinqtcurverc')
