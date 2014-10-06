e161ca9c2a90f5c67086ba0581f5a257e0af6ae4
Victor Varvaryuk <victor.varvariuc@gmail.com>
2014-10-06 12:19:20 +0400

    Update qtcurve theme

--- packages/vic/qtcurve_vic/__init__.py
+++ packages/vic/qtcurve_vic/__init__.py
@@ -24,6 +24,7 @@ lines as possible - to make it visually light.<br>
         self.copy_file('./vic.qtcurve', '~/.kde/share/apps/QtCurve/')
         self.copy_file('./vic_dark.colors', '~/.kde/share/apps/color-schemes/')
         self.update_kconfig('./stylerc', '~/.config/qtcurve/stylerc')
+        self.copy_file('./windowBorderSizes', '~/.config/qtcurve/')
         self.update_kconfig('./kwinrc', '~/.kde/share/config/kwinrc')
         self.update_kconfig('./kwinqtcurverc', '~/.kde/share/config/kwinqtcurverc')
         self.update_kconfig('./kdeglobals', '~/.kde/share/config/kdeglobals')
