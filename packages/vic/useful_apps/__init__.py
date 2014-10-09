c6907e36522998dcf6cbed1cec192fdd0f803425
Victor Varvaryuk <victor.varvariuc@gmail.com>
2014-10-09 11:19:46 +0400

    Removed overlay-scrollbar from useful apps

--- packages/vic/useful_apps/__init__.py
+++ packages/vic/useful_apps/__init__.py
@@ -4,14 +4,12 @@ from mykde import BaseAction
 class Action(BaseAction):
 
     name = "Useful apps"
-    packages = ['p7zip-full', 'speedcrunch', 'kolourpaint4', 'pavucontrol', 'overlay-scrollbar',
-                'kwrite']
+    packages = ['p7zip-full', 'speedcrunch', 'pavucontrol', 'kwrite']
     description = """
 Some useful applications:
 <ul>
-    <li>7-zip archiver libs</li>
+    <li>7-zip archiver</li>
     <li>SpeedCrunch calculator</li>
-    <li>KolourPaint image editor</li>
     <li>...</li>
 </ul>
 Full list: %s
