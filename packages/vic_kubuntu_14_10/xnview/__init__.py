01c2f617d8556e279870d32247d07216a49a41da
Victor <victor.varvariuc@gmail.com>
2015-06-21 12:57:06 +0300

    Added file associations for XnView.

--- packages/vic_kubuntu_14_10/xnview/__init__.py
+++ packages/vic_kubuntu_14_10/xnview/__init__.py
@@ -14,3 +14,18 @@ XnViewMP is a powerful cross-platform media browser, viewer and converter, suppo
         'ppa:dhor/myway': ('', ''),
     }
     packages = ['xnview']
+
+    def proceed(self):
+        self.print_html('<b>Associate image fiels with XnView, because the package does not do this</b>')
+        commands = """\
+xdg-mime default xnview.desktop image/jpeg
+xdg-mime default xnview.desktop image/png
+xdg-mime default xnview.desktop image/bmp
+xdg-mime default xnview.desktop image/tiff
+xdg-mime default xnview.desktop image/x-portable-anymap
+xdg-mime default xnview.desktop image/x-portable-bitmap
+xdg-mime default xnview.desktop image/x-portable-graymap
+xdg-mime default xnview.desktop image/x-portable-pixmap
+"""
+        for command in commands.splitlines():
+            self.call(command)
